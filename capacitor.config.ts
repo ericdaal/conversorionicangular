7cb435a1515dea34d1aef80919d7da660eb7e5a0
ealvaMyNube <115048443+ealvaMyNube@users.noreply.github.com>
2023-11-20 11:56:33 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'DivisApp_1120031',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
